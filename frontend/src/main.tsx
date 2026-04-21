376857b85027db50dceeca7c3eb13ee4b7249215
1 <a1@1ui-Macmini.local>
2026-04-21 14:43:41 +0900

    맥미니 터미널에서 직접 수정 및 배포: 2026-04-21 14:43:41

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createNetworkConfig, SuiClientProvider, WalletProvider } from "@mysten/dapp-kit";
-// 에러의 원인이었던 getFullnodeUrl import를 아예 삭제합니다!
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Theme } from "@radix-ui/themes";
+import { EnokiFlowProvider } from "@mysten/enoki/react";
 import "@mysten/dapp-kit/dist/index.css"; 
 import "@radix-ui/themes/styles.css";
 import App from "./App.tsx";
@@ -11,21 +11,26 @@ import "./index.css";
 
 const queryClient = new QueryClient();
 
-// 함수를 쓰지 않고 테스트넷 RPC 주소를 직접 명시합니다. (완벽한 우회 방법)
 const { networkConfig } = createNetworkConfig({
-	testnet: { url: "https://fullnode.testnet.sui.io:443" },
+testnet: { url: "https://fullnode.testnet.sui.io:443" },
 });
 
+// TODO: 반드시 'enoki_public_key_' 로 시작하는 공개 키를 넣으세요!
+const ENOKI_API_KEY = "enoki_private_07a009182017b289c1ac6b7307f504a3";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-	<React.StrictMode>
-		<Theme appearance="dark">
-			<QueryClientProvider client={queryClient}>
-				<SuiClientProvider networks={networkConfig} defaultNetwork="testnet">
-					<WalletProvider autoConnect>
-						<App />
-					</WalletProvider>
-				</SuiClientProvider>
-			</QueryClientProvider>
-		</Theme>
-	</React.StrictMode>,
+<React.StrictMode>
+<Theme appearance="dark">
+<QueryClientProvider client={queryClient}>
+<SuiClientProvider networks={networkConfig} defaultNetwork="testnet">
+<WalletProvider autoConnect>
+                        {/* 바로 이 부분이 App을 감싸주어야 에러가 사라집니다! */}
+                        <EnokiFlowProvider apiKey={ENOKI_API_KEY}>
+    <App />
+                        </EnokiFlowProvider>
+</WalletProvider>
+</SuiClientProvider>
+</QueryClientProvider>
+</Theme>
+</React.StrictMode>,
 );
