69ac48447213bf197de6f366ed47c96569aba481
pky060921 <pky060921@gmail.com>
2026-04-24 13:07:23 +0900

    Integrate SuiClientProvider and QueryClientProvider

--- blankd-app/src/main.tsx
+++ blankd-app/src/main.tsx
@@ -2,9 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { createNetworkConfig, SuiClientProvider, WalletProvider } from '@mysten/dapp-kit';
+import { getFullnodeUrl } from '@mysten/sui/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import '@mysten/dapp-kit/dist/index.css';
+
+const queryClient = new QueryClient();
+const { networkConfig } = createNetworkConfig({
+	testnet: { url: getFullnodeUrl('testnet') },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<SuiClientProvider networks={networkConfig} defaultNetwork="testnet">
+				<WalletProvider autoConnect>
+					<App />
+				</WalletProvider>
+			</SuiClientProvider>
+		</QueryClientProvider>
+	</React.StrictMode>,
 )
