469309579336bba0a6a97064375c1df666481542
pky060921 <pky060921@gmail.com>
2026-05-04 22:50:39 +0900

    Update main.tsx

--- blankd-app/src/main.tsx
+++ blankd-app/src/main.tsx
@@ -7,8 +7,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { EnokiFlowProvider } from '@mysten/enoki/react'
 
 const queryClient = new QueryClient()
-
-// 💡 에러를 일으키던 getFullnodeUrl 함수를 지우고, 메인넷 공식 주소를 직접 꽂아 넣었습니다! (에러율 0%)
+// getFullnodeUrl 함수 충돌을 막기 위해 메인넷 주소 하드코딩
 const networks = { mainnet: { url: 'https://fullnode.mainnet.sui.io:443' } }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -16,7 +15,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <SuiClientProvider networks={networks} defaultNetwork="mainnet">
         <WalletProvider>
-          {/* 아키님의 Enoki Public Key */}
           <EnokiFlowProvider apiKey="enoki_public_79288e2c949704c77c61148439df67d1"> 
             <App />
           </EnokiFlowProvider>
